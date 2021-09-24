b01daf88ca4c6ab35ecdac5c4d9d772385abedd2
arsenkakheli <lestor11994@gmail.com>
2021-09-24 23:48:43 +0300

    commited

---
+++ homework 6/homework 61.py
@@ -0,0 +1,11 @@
+# f = open("homework 661.txt",'w+')
+# f = open("homework 6661.txt",'w+')
+with open("homework 661.txt","w+") as file:
+    s = file.read()
+    f = s.count(".")
+
+print(f)
+
+# for i in range(10):
+#     f.write()
+
