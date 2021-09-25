ac44e078b1a03a4d9957998a6c610aaa43ffbf04
arsenkakheli <lestor11994@gmail.com>
2021-09-25 11:10:09 +0300

    commited

--- homework 6/homework 61.py
+++ homework 6/homework 61.py
@@ -1,11 +1,25 @@
-# f = open("homework 661.txt",'w+')
-# f = open("homework 6661.txt",'w+')
-with open("homework 661.txt","w+") as file:
-    s = file.read()
-    f = s.count(".")
+with open("homework 661.txt", "w+", encoding="utf-8") as file:
+    file.write('Hello! My name is Arsen! Nice to meet you! Currently, I"m residing in Kyiv city and working as a Data\n'
+'Mining specialist in Influ2, inc. For this moment I"m studying Python language ane doing this task.\n'
+'I"m going to be a Data Engineer. That"s why I started to learn this adorable language. It"s quite difficult, but I"m\n'
+'doing my best. I want to automatize some processes to create the comfortable conditions from my department. As I"m working\n'
+'with data, I need to learn more than Python. BTW, firstly I need to pass the basic course of Python development\n'
+'Wish me good luck, as I"m going to learn something difficult and interesting in my life!!')
 
-print(f)
+ffile = open("homework 661.txt", "r")
 
-# for i in range(10):
-#     f.write()
+rows_num = 0
+words_num = 0
+chr_num = 0
+for line in ffile:
+  row = line.strip("\n")
 
+  words = line.split()
+  rows_num += 1
+  words_num += len(words)
+  chr_num += len(row)
+
+print("This file contains ", rows_num," rows, ", words_num, " words and ",chr_num, " characters")
+
+
+# print(str(q) + str(row) + str(dots))
